7d3e977c712ac83013603da4e76bb107fecd8d5d
dr_one <studmm@ukr.net>
2020-11-17 23:07:24 +0200

    no message

---
+++ code/public/Collections/SumN.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace Collections
+{
+    public class SumN
+    {
+        public int N { get; set; } = 1 * 1000 * 1000;
+
+        public long Solution1()
+        {
+            return Enumerable.Range(1, N)
+                .ToList()
+                .Aggregate((x, y) => x + y);
+        }
+    }
+}
